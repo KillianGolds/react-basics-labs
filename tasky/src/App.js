287fbf9b46de261d4d29883841004dec4671bb7d
KillianGolds <20070585@mail.wit.ie>
2023-09-28 19:39:50 +0100

    Completed Week 3B Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import Task from './components/Task';
 import AddTaskForm from './components/Form';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
   const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", done: false },
-      { id: 4, title: "Grocery shopping", description: "Buy groceries for the week",  deadline: "Next week", done: false },
-      { id: 5, title: "Write report", description: "Complete the project report", deadline: "Friday", done: false },
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
+      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", priority: "High", done: false },
+      { id: 4, title: "Grocery shopping", description: "Buy groceries for the week",  deadline: "Next week", priority: "Low", done: false },
+      { id: 5, title: "Write report", description: "Complete the project report", deadline: "Friday", priority: "Low", done: false },
     ]
   });
 
@@ -24,64 +27,92 @@ function App() {
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
-    setTaskState({tasks});
-  } 
+    setTaskState({ tasks });
+  }
 
-  const [ formState, setFormState ] = useState({
+  const [formState, setFormState] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: "Low",
   });
 
   const formChangeHandler = (event) => {
-    let form = {...formState};
-
-    switch(event.target.name) {
-      case "title":
-          form.title = event.target.value;
-          break;
-      case "description":
-          form.description = event.target.value;
-          break;
-      case "deadline":
-          form.deadline = event.target.value;
-          break;
-      default:
-          form = formState;
-    }
-    setFormState(form);
-    console.log(formState);
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   }
+  
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
     const tasks = [...taskState.tasks];
-    const form = {...formState};
+    const form = { ...formState };
 
     form.id = uuidv4();
-    
+
     tasks.push(form);
-    setTaskState({tasks});
+    setTaskState({ tasks });
   }
 
   return (
     <div className="container">
-    <h1>Tasky</h1>
-    {taskState.tasks.map((task, index) => (              
-      <Task 
-        title={task.title}
-        description={task.description}
-        deadline={task.deadline}
-        key={task.id}
-        done={task.done}
-        markDone={() => doneHandler(index)}
-        deleteTask = {() => deleteHandler(index)}
-      />
-    ))}
-    <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
-  </div>
-
+      {/* App Header */}
+      <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx={{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
+      {/* End App Header */}
+      {/* Task Card Grid */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-top" justifyContent="center">
+          {taskState.tasks.map((task, index) => (
+            <Task
+              title={task.title}
+              description={task.description}
+              deadline={task.deadline}
+              priority={task.priority}
+              done={task.done}
+              key={task.id}
+              markDone={() => doneHandler(index)}
+              deleteTask={() => deleteHandler(index)}
+            />
+          ))}
+        </Grid>
+      </Container>
+      {/* End Task Card Grid */}
+      {/* Footer - Add Task Form */}
+      <Container
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          my: 6,
+          py: 6,
+        }}
+      >
+        <Grid container justifyContent="center">
+          <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+        </Grid>
+      </Container>
+      {/* End Footer */}
+    </div>
   );
 }
 
--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -1,31 +1,89 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
 
 const AddTaskForm = (props) => {
+  const labelStyles = {
+    shrink: true,
+    style: {
+      fontSize: '16px',
+      marginRight: '10px',
+      marginBottom: '5px',
+      marginLeft: '5px',
+    },
+  };
 
   return (
-    <div>
-      <form onSubmit={props.submit}>
-      <label>
-        Task title:
-        <input type="text" name="title" required onChange={(event) => props.change(event)} />
-    </label>
-    <br />
-    <label>
-        Due date:
-        <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
-    </label>
-    <br />
-    <label>
-        Details:
-        <input type="text" name="description" onChange={(event) => props.change(event)} />
-    </label>
-
-        <input type="submit" value="Submit" />
-        </form>
-    </div>
-  )
+    <Box
+      component="form"
+      sx={{
+        '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+      }}
+      onSubmit={props.submit}
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          name="title"
+          label="Task Title"
+          InputLabelProps={labelStyles}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          required
+          name="deadline"
+          label="Deadline"
+          InputLabelProps={labelStyles}
+          type="date"
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-select-priority"
+          select
+          name="priority" // Updated the name to "priority"
+          label="Priority"
+          InputLabelProps={labelStyles}
+          onChange={(event) => props.change(event)}
+        >
+          <MenuItem value="Low">Low</MenuItem>
+          <MenuItem value="Medium">Medium</MenuItem>
+          <MenuItem value="High">High</MenuItem>
+        </TextField>
+      </div>
+      <div>
+        <TextField
+          name="description"
+          id="outlined-multiline-static"
+          label="Task Details"
+          InputLabelProps={labelStyles}
+          multiline
+          rows={4}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <Button 
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            m: 1,
+            p: 1,
+            width: '95%'
+          }}
+        >
+          Add Task
+        </Button>
+      </div>
+    </Box>
+  );
 };
-
-
 
 export default AddTaskForm;
